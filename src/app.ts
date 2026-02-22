2a9b9571b159f47fc6c2691c7f9e8c1082a3a967
Alejandro Pineda <alejandropineda41310@gmail.com>
2026-02-22 11:24:07 -0500

    Backend preparado para completar login con google y supabase

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,5 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
+import express, { Express } from 'express';
 
-dotenv.config();
-
-const app = express();
-
-app.use(helmet());
-app.use(cors({ origin: '*' }));  // Ajusta para prod
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend UniConnect OK' });
-});
+const app: Express = express();
 
 export default app;
